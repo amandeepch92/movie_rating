d2f93a0d11f79db02505c9bdcd9da28c34c7c3f1
Amandeep Singh <amasingh18@publicisgroupe.net>
2018-08-21 13:10:12 +0530

    new component added

--- src/components/AddMovie.vue
+++ src/components/AddMovie.vue
@@ -16,10 +16,14 @@
       label="Movie Release Year"
       v-model="release_year"
       :items="years"
+      required
+      :rules="releaseRules"
     ></v-select>
     <v-text-field
       label="Movie Genre"
       v-model="genre"
+      required
+      :rules="genreRules"
     ></v-text-field>
     <v-btn
       @click="submit"
@@ -30,8 +34,9 @@
     <v-btn @click="clear">clear</v-btn>
   </v-form>
 </template>
-
 <script>
+import axios from 'axios';
+
 export default {
   data: () => ({
     valid: true,
@@ -41,6 +46,13 @@ export default {
     release_year: '',
     nameRules: [
       v => !!v || 'Movie name is required',
+    ],
+    genreRules: [
+      v => !!v || 'Movie genre year is required',
+      v => (v && v.length <= 80) || 'Genre must be less than equal to 80 characters.',
+    ],
+    releaseRules: [
+      v => !!v || 'Movie release year is required',
     ],
     select: null,
     years: [
@@ -53,8 +65,37 @@ export default {
   methods: {
     submit() {
       if (this.$refs.form.validate()) {
-        // Perform next action
+        return axios({
+          method: 'post',
+          data: {
+            name: this.name,
+            description: this.description,
+            release_year: this.release_year,
+            genre: this.genre,
+          },
+          url: 'http://localhost:8081/movies',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+          .then(() => {
+            this.$swal(
+              'Great!',
+              'Movie added successfully!',
+              'success',
+            );
+            this.$router.push({ name: 'Home' });
+            this.$refs.form.reset();
+          })
+          .catch(() => {
+            this.$swal(
+              'Oh oo!',
+              'Could not add the movie!',
+              'error',
+            );
+          });
       }
+      return true;
     },
     clear() {
       this.$refs.form.reset();
