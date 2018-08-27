942d790ac7b51d69dad7e6270d472da38f35d046
Amandeep Singh <amasingh18@publicisgroupe.net>
2018-08-27 15:55:30 +0530

    login using google and facebook added

--- config/Config.js
+++ config/Config.js
@@ -1,4 +1,8 @@
 module.exports = {
   DB: 'mongodb://localhost/movie_rating_app',
   SECRET: 'movieratingappsecretkey',
+  FACEBOOK_APP_ID: '560823737710172',
+  FACEBOOK_APP_SECRET: '82ccee53fcd1bbc31498ad76959c00a1',
+  GOOGLE_APP_ID: '635583779301-30hlunb04ehcleacm71hr7d12gqcggnt.apps.googleusercontent.com',
+  GOOGLE_APP_SECRET: '5jXr712WI9z8CjZ9tP6DLH9K',
 }
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,24 +1,32 @@
 <template>
-  <v-form v-model="valid" ref="form" lazy-validation>
-    <v-text-field
-      label="Email"
-      v-model="email"
-      :rules="emailRules"
-      required
-    ></v-text-field>
-    <v-text-field
-      label="Password"
-      v-model="password"
-      required
-    ></v-text-field>
-    <v-btn
-      @click="submit"
-      :disabled="!valid"
-    >
-      submit
-    </v-btn>
-    <v-btn @click="clear">clear</v-btn>
-  </v-form>
+  <div>
+    <div class="login">
+        <a class="btn facebook" href="/login/facebook"> LOGIN WITH FACEBOOK</a>
+        <a class="btn twitter" href="/login/twitter"> LOGIN WITH TWITTER</a>
+        <a class="btn google" href="/login/google"> LOGIN WITH GOOGLE</a>
+        <a class="btn linkedin" href="/login/linkedin"> LOGIN WITH LINKEDIN</a>
+    </div>
+    <v-form v-model="valid" ref="form" lazy-validation>
+      <v-text-field
+        label="Email"
+        v-model="email"
+        :rules="emailRules"
+        required
+      ></v-text-field>
+      <v-text-field
+        label="Password"
+        v-model="password"
+        required
+      ></v-text-field>
+      <v-btn
+        @click="submit"
+        :disabled="!valid"
+      >
+        submit
+      </v-btn>
+      <v-btn @click="clear">clear</v-btn>
+    </v-form>
+  </div>
 </template>
 <script>
 import axios from 'axios';
