3e8e56e7de7236985c719563d88fd54a0ba42c65
Amandeep Singh <aman@Amandeeps-MacBook-Air.local>
2018-08-30 20:55:00 +0530

    mongo updated

--- config/Config.js
+++ config/Config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  DB: 'mongodb://aman:Newpass@123@ds157509.mlab.com:57509/movie_rating_app',
+  DB: 'mongodb://aman:test123@ds018308.mlab.com:18308/movie_rating_app',
   SECRET: 'movieratingappsecretkey',
   FACEBOOK_APP_ID: '560823737710172',
   FACEBOOK_APP_SECRET: '82ccee53fcd1bbc31498ad76959c00a1',
